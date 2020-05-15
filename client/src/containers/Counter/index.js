ca219614615901fbbb4cc7f2523fdc5cecf9dd84
Sergio Di Martino <sergio@webdevserg.io>
2020-05-15 11:17:53 -0700

    Cleans up code

--- client/src/containers/Counter/index.js
+++ client/src/containers/Counter/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Header, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { increment } from '../../actions/counterActions';
-import { decrement } from '../../actions/counterActions';
+import { increment, decrement } from '../../actions/counterActions';
 
 class Counter extends Component {
   render() {
