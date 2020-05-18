28a5a088f56d112e28e7fd0b9cdd8c8cfad06c4d
Sergio Di Martino <sergio@webdevserg.io>
2020-05-18 11:49:00 -0700

    Reconfigures navbar when logged in

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 700 }}>
-          <Navbar/>
+          <Navbar authenticated={ this.props.authenticated }/>
           <Route exact path='/' component={SignUp}/>
           <Route exact path='/alltodos' component={ AllTodosList } />
           <Route exact path='/counter' component={ Counter } />
