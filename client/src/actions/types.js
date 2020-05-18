8ae23aeee8305cecdb1f3736715c807bf0b19c42
Sergio Di Martino <sergio@webdevserg.io>
2020-05-18 10:26:54 -0700

    Adds authReducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,5 @@ export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const GET_ALL_TODOS = 'GET_ALL_TODOS';
 export const GET_ALL_TODOS_ERROR = 'GET_ALL_TODOS_ERROR';
+export const AUTH_USER = 'AUTH_USER';
+export const AUTH_USER_ERROR = 'AUTH_USER_ERROR';
