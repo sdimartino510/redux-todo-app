dae72a49e929534cdeebfee4f0edba3f3caa5d35
Sergio Di Martino <sergio@webdevserg.io>
2020-05-19 14:09:45 -0700

    Adds hoc

---
+++ client/src/hoc/requireAuth/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default (ChildComponent) => {
+  class ComposedComponent extends Component {
+    componentDidMount() {
+      this.shouldNavigateAway();
+    }
+
+    componentDidUpdate() {
+      this.shouldNavigateAway();
+    }
+
+    shouldNavigateAway() {
+      if (!this.props.authenticated) {
+        this.props.history.push('/');
+      }
+    }
+
+    render() {
+      return <ChildComponent { ...this.props } />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated }
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
+}
